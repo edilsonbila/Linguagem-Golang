09120910d0be4c0f2e9bc028c693539c82c774eb
edilsonbila <142514795+edilsonbila@users.noreply.github.com>
2025-02-09 00:30:09 -1200

    #Feature
    converter os tipos de dados

---
+++ conversaodetipo_De_dados.go
@@ -0,0 +1,19 @@
+package main
+import "fmt" 
+
+// Conversão de tipos
+func main() {
+
+	var a int = 46
+	var b float64 = 2.3
+
+    var d float64 = float64(a)//convertendo a para float64
+	var c int = int(b)// converte b para inteiro
+	fmt.Println("valor de c e",c)
+	fmt.Println("valor de d e",d)
+	//para converter e so dizer o tipo e dentro de parentes coloca avariavel
+
+
+
+
+}
