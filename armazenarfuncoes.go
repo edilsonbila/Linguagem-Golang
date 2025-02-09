fbd8997706e2221f4a367b193c2857f36298e8a4
edilsonbila <142514795+edilsonbila@users.noreply.github.com>
2025-02-09 00:25:36 -1200

    #Featuring
    como armazenar funcoes em variaveis

---
+++ armazenarfuncoes.go
@@ -0,0 +1,26 @@
+package main
+import "fmt"
+
+func somar(a float64,b float64) float64 {
+
+	return a + b
+}
+func main(){
+
+
+  
+ fmt.Println(somar(3 , 4))
+/**
+outra forma seria 
+
+somar:= func (a ,b float64) float64 {
+
+return a+b }
+fmt.println(3,4)//tudo no main}
+
+
+
+**/
+
+
+}
