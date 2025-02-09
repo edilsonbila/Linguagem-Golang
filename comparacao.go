fdd821d20de85567570625a02e5878e7cc09e6e6
edilsonbila <142514795+edilsonbila@users.noreply.github.com>
2025-02-09 00:28:38 -1200

    #Feature
    comparacao de valores

---
+++ comparacao.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main() {
+var a int =27
+var b int =7
+
+fmt.Println("a>b",a > b)//saida sera true 
+fmt.Println("a <b",a < b)//saida sera false
+
+fmt.Println("a>=b",a >= b)//saida sera true porque na parete maior e verdadeira 
+//e como setrata de uma  comparacao ou so e falso se os dois forem falsos
+fmt.Println("a == b",a == b)// saida sera false por
+}
