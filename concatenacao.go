284b21d8c6d4d44471150a9f5137a46cc17b2c13
edilsonbila <142514795+edilsonbila@users.noreply.github.com>
2025-02-09 00:29:07 -1200

    #feature
    concatenacao

---
+++ concatenacao.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main() {
+ palavra1 := "ola"
+ palavra2 := "mundo"
+ fmt.Println(palavra1 + " " + palavra2)
+//ai concatena as strings imprimindo ola mundo
+
+
+
+}
